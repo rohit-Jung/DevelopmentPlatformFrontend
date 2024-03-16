b3042a22f1ab2c454ae429c635776997f5457866
rohit-Jung <rokshh28@gmail.com>
2024-03-16 08:57:42 +0545

    Fixed some styling

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -12,15 +12,15 @@ function Sorting() {
   };
 
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col gap-2">
       <button
-        className="w-1/2 rounded-l-lg px-5 py-1 bg-red-700 text-white shrink-0"
+        className="w-full rounded-lg px-5 py-1 bg-red-700 text-white shrink-0 hover:bg-red-950 hover:text-gray-100"
         onClick={deleteAllHandler}
       >
         Delete all
       </button>
       <button
-        className="w-1/2 rounded-r-lg px-5 py-1 bg-green-800 text-white shrink-0"
+        className="w-full rounded-lg px-5 py-1 bg-green-800 text-white shrink-0 hover:bg-green-950 hover:text-gray-100"
         onClick={deleteCompletedHandler}
       >
         Delete Completed
